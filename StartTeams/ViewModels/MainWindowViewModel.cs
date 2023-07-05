6755fa56b0e5fc6e6498260188b976e5fe1273d6
Patrick Robin <patrick-robin@gmx.net>
2023-07-06 00:34:34 +0200

    Projektdateien hinzufügen.

---
+++ StartTeams/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿/*
+*----------------------------------------------------------------------------------
+*          Filename:	MainWindowViewModel.cs
+*          Date:        2023.07.05
+*          All rights reserved
+*
+*----------------------------------------------------------------------------------
+* @author Patrick Robin <support@rietrob.de>
+*/
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+
+namespace StartTeams.ViewModels;
+[ObservableObject]
+public partial class MainWindowViewModel 
+{
+    #region Fields
+    public string ApplicationPath { get; set; }
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Constructor
+
+    #endregion
+
+    #region Methods
+
+    private string getApplicationpath()
+    {
+
+        using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall\Teams"))
+        {
+            var microsoftTeamsExecuteablePath = registryKey?.GetValue("InstallLocation")?.ToString();
+            if (microsoftTeamsExecuteablePath != null)
+            {
+                var microsoftTeamsExecutable = Path.Combine(microsoftTeamsExecuteablePath, "current", "Teams.exe");
+                if (File.Exists(microsoftTeamsExecutable))
+                {
+                    return microsoftTeamsExecutable;
+                }
+            }
+        }
+
+        return null;
+    }
+    #endregion
+
+    #region Commands
+
+    [RelayCommand]
+    private void StartTeams()
+    {
+        Process newProcess = null;
+        if (!string.IsNullOrEmpty(ApplicationPath))
+        {
+            newProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo(getApplicationpath())
+                {
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    CreateNoWindow = false
+                }
+            };
+            newProcess.Start();
+        }
+    }
+    [RelayCommand]
+    private void ExitApp()
+    {
+        Environment.Exit(0);
+    }
+    #endregion
+}
---
+++ StartTeams/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows;
+
+using StartTeams.ViewModels;
+
+namespace StartTeams.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
